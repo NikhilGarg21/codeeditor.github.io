4e8bada5acffe8429fecfaaa9cd2515490aa2b04
NikhilGarg21 <146012122+NikhilGarg21@users.noreply.github.com>
2024-11-20 14:42:23 +0530

    Update Api.js

--- Api.js
+++ Api.js
@@ -1,31 +1,42 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 const bodyP = require("body-parser");
 const compiler = require("compilex");
 const cors = require("cors");
 
+const app = express();
 const options = { stats: true };
 compiler.init(options);
 
 app.use(bodyP.json());
 app.use(cors());
 
-// Remove the local CodeMirror static file serving, as it's no longer needed
-// app.use(
-//   "/codemirror/codemirror-5.65.18",
-//   express.static(
-//     "C:/Users/ngarg/Downloads/CodeEditor/codemirror/codemirror-5.65.18"
-//   )
-// );
-
-// Serve the index.html (frontend)
+// Serve the index.html (frontend) from the project directory
 app.get("/", function (req, res) {
-  compiler.flush(function () {
-    console.log("deleted");
-  });
-  // Adjust this path to where your 'index.html' is actually located
-  res.sendFile("C:/Users/ngarg/Downloads/CodeEditor/index.html");
+  // Dynamically serve the index.html file
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+// Generic function to compile code based on language
+function compileCode(envData, code, input, compileFunc, compileFuncWithInput, res) {
+  if (!input) {
+    compileFunc(envData, code, function (data) {
+      if (data.output) {
+        return res.send(data);
+      } else {
+        return res.send({ output: "error" });
+      }
+    });
+  } else {
+    compileFuncWithInput(envData, code, input, function (data) {
+      if (data.output) {
+        return res.send(data);
+      } else {
+        return res.send({ output: "error" });
+      }
+    });
+  }
+}
 
 // Compilation endpoint
 app.post("/compile", function (req, res) {
@@ -36,69 +47,28 @@ app.post("/compile", function (req, res) {
   try {
     let envData;
 
-    // Handle different language compilations
-    if (lang === "Cpp" || lang === "C") {
-      envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
-      if (!input) {
-        compiler.compileCPP(envData, code, function (data) {
-          if (data.output) {
-            return res.send(data);
-          } else {
-            return res.send({ output: "error" });
-          }
-        });
-      } else {
-        compiler.compileCPPWithInput(envData, code, input, function (data) {
-          if (data.output) {
-            return res.send(data);
-          } else {
-            return res.send({ output: "error" });
-          }
-        });
-      }
-    } else if (lang === "Java") {
-      envData = { OS: "windows" };
-      if (!input) {
-        compiler.compileJava(envData, code, function (data) {
-          if (data.output) {
-            return res.send(data);
-          } else {
-            return res.send({ output: "error" });
-          }
-        });
-      } else {
-        compiler.compileJavaWithInput(envData, code, input, function (data) {
-          if (data.output) {
-            return res.send(data);
-          } else {
-            return res.send({ output: "error" });
-          }
-        });
-      }
-    } else if (lang === "Python") {
-      envData = { OS: "windows" };
-      if (!input) {
-        compiler.compilePython(envData, code, function (data) {
-          if (data.output) {
-            return res.send(data);
-          } else {
-            return res.send({ output: "error" });
-          }
-        });
-      } else {
-        compiler.compilePythonWithInput(envData, code, input, function (data) {
-          if (data.output) {
-            return res.send(data);
-          } else {
-            return res.send({ output: "error" });
-          }
-        });
-      }
-    } else {
-      return res.status(400).send({ error: "Unsupported language" });
+    switch (lang) {
+      case "Cpp":
+      case "C":
+        envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
+        compileCode(envData, code, input, compiler.compileCPP, compiler.compileCPPWithInput, res);
+        break;
+
+      case "Java":
+        envData = { OS: "windows" };
+        compileCode(envData, code, input, compiler.compileJava, compiler.compileJavaWithInput, res);
+        break;
+
+      case "Python":
+        envData = { OS: "windows" };
+        compileCode(envData, code, input, compiler.compilePython, compiler.compilePythonWithInput, res);
+        break;
+
+      default:
+        return res.status(400).send({ error: "Unsupported language" });
     }
   } catch (error) {
-    console.log("Error", error);
+    console.log("Error:", error);
     if (!res.headersSent) {
       return res.status(500).send({ error: "Internal Server Error" });
     }
